f7135c7449ccac9e022dd1a295f92920d6e7f986
pacoalm <pacoalm@gmail.com>
2022-03-21 19:55:26 +0000

    Primer Commit

---
+++ console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace console
+{
+    class Producto {
+        public string Nombre {get;set;}
+        public decimal Precio {get;set;}
+        public string Descripcion {get;set;}
+    }
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+             Producto prod = new Producto 
+            {
+                Nombre = "Camisa",
+                Precio = 10,
+                Descripcion="Tela de algodon"
+            };
+            string json = JsonConvert.SerializeObject(prod, Formatting.Indented);
+            Console.WriteLine(json);
+        }
+    }
+}
---
+++ webapi/Solution1/ProductAPI/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProductAPI.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public double Price { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public double Rating { get; set; }
+    }
+}
